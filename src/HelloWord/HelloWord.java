5774f5f70d45a88de1aec303882d2f36a89d128a
huy2x <dangquochuy.se@gmail.com>
2024-11-28 15:11:03 +0700

    init project

---
+++ src/HelloWord/HelloWord.java
@@ -0,0 +1,10 @@
+package HelloWord;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWord");
+		System.out.println("HelloWord");
+		System.out.println("HelloWord");
+	}
+}

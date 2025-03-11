f0570ff965b53145f1aed1403b302ae84d896d56
huy2x <dangquochuy.se@gmail.com>
2025-03-11 15:38:03 +0700

    buoi2

--- src/HelloWord/HelloWord.java
+++ src/HelloWord/HelloWord.java
@@ -1,10 +1,32 @@
 package HelloWord;
 
+import java.util.Scanner;
+
 public class HelloWord {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWord");
-		System.out.println("HelloWord");
-		System.out.println("HelloWord");
+		System.out.println("Hello Word");
+		Scanner scanner = new Scanner(System.in);
+		// Bài 1
+//		System.out.println("Nhập tên sv: ");
+//		String name = scanner.nextLine();
+//		System.out.println("Nhập điểm tb: ");
+//		String point = scanner.nextLine();
+//		System.out.println("Sinh viên: " + name + " Có điểm: " + point);
+
+		// bài 2
+//		System.out.println("Nhập vào chiều rộng: ");
+//		Double width = scanner.nextDouble();
+//		System.out.println("Nhập vào chiều dài: ");
+//		Double length = scanner.nextDouble();
+//		System.out.println("Chu vi hình chữ nhật là: " + (width + length) * 2);
+//		System.out.println("Diện tích hình chữ nhật là: " + (width * length));
+//		System.out.println("Cạnh nhỏ nhất là: " + Math.min(width, length));
+
+		// bài 3
+		System.out.println("Nhập vào cạnh của khối lập phương: ");
+		Double edge = scanner.nextDouble();
+		System.out.println("Thể tích khối lập phương là: " + Math.pow(edge, 3));
+		scanner.close();
 	}
 }
